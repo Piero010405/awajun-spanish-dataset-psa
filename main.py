9a892e9f41150e2d7b0ec0dff64c2c24e903c1fb
Piero010405 <psarmientoayala@gmail.com>
2025-10-30 02:39:06 -0500

    Old Testament Diccionary

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ from src import config
 
 def main():
     for book, info in BOOKS.items():
-        for ch in range(1, info["chapters"] + 1):
+        chapters = info["chapters"]
+
+        # Validacion por si tenemos Libros completos o solo numero de capitulos
+        if isinstance(chapters, int):
+            chapters = range(1, chapters + 1)
+
+        for ch in chapters:
             for lang in ["awajun", "spanish"]:
                 url = build_url(lang, book, ch)
                 print(f"Descargando {book} {ch} ({lang}) → {url}")
+
                 verses = get_verses(url, lang, book, ch, timeout=config.TIMEOUT)
                 if verses:
                     save_verses(verses, lang, book, ch)
@@ -17,6 +24,7 @@ def main():
     print("Construyendo dataset final...")
     build_dataset()
     print("✅ Dataset generado correctamente.")
+
 
 if __name__ == "__main__":
     main()
